d6484fb3bf188d4b15486003202d792a6b9a24aa
2024dc04110-dotcom <2024dc04110@wilp.bits-pilani.ac.in>
2026-01-31 00:20:35 +0530

    Change import from LogisticRegressionModel to logistic

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Import models
-from model.logistic_regression import LogisticRegressionModel
+from model.logistic_regression import logistic
 
 MODEL_MAP = {
     "Logistic Regression": logistic,
